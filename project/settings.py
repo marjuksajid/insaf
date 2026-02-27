5066694d9adf0b0b74b3fc84b533979a45e57cd9
marjuksajid <marjuksajid@protonmail.com>
2026-02-27 08:45:33 +0600

    Remove default SECRET_KEY from settings for enhanced security

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1(9ap!koa1(7%%hos@=&=ty5p8-7i9)d*39n!&8i6c51^_)t@3')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
